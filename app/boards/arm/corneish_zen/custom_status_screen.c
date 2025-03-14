b3ec6d593f22d677cc1421e322fa839ba0aa1893
Tim Stewart <timstewartj@gmail.com>
2025-03-13 23:23:34 -0700

    feat(corneish_zen): Add conditional display of timlogo based on configuration

--- app/boards/arm/corneish_zen/custom_status_screen.c
+++ app/boards/arm/corneish_zen/custom_status_screen.c
@@ -108,6 +108,14 @@ lv_obj_t *zmk_display_status_screen() {
 #else
     lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, -5);
 #endif
+#else
+#if !IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
+    lv_obj_t *zenlogo_icon;
+    zenlogo_icon = lv_img_create(screen);
+    LV_IMG_DECLARE(timlogo);
+    lv_img_set_src(zenlogo_icon, &timlogo);
+    lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, -5);
+#endif
 #endif
 
     return screen;
